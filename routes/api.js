cd851c519b0d57794c3557542814c4418aac314c
Nathan Bland <nathan.bland@gmail.com>
2015-04-27 18:05:46 -0500

    Update to capture events and parameters from pianobar

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var spawn = require('child_process').spawn;
 var pianobar = spawn('pianoctl');
+var stationList = []
 exports.setup = function() {
     var router = express.Router();
 	router.get('/pause', function(req,res,next){
@@ -29,6 +30,26 @@ exports.setup = function() {
 	});
 	router.get('/down', function(req,res,next){
 		pianobar.stdin.write("-\n");
+		return res.json({
+			status: 200
+		});
+	});
+	router.get('/watch', function(req,res,next){
+		//console.log(req.query);
+		params = req.query;
+		console.log('Event! type:', params.type);
+		console.log("song:",params.title,"artist:",params.artist);
+		if (params.type == 'songlove'){
+			console.log("YOU LOVE THAT SONG GOOD");
+		}
+		//console.log(params);
+		/*for (idx in params){
+			//console.log(idx,":",params[idx]);
+			if (idx.toString().indexOf("station") > -1){
+				//console.log(params[idx]);
+				stationList[idx] = params[idx];
+			}
+		}*/
 		return res.json({
 			status: 200
 		});
